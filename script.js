f8358506413a72137936c9a027b1981109b7f5d6
gatwi <faith.murithi@student.moringaschool.com>
2022-06-23 09:12:53 +0300

    commit

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 // making the navigation system come alive
-const links = document.querySelectorAll('.link');
-const sections = document.querySelectorAll('section');
+// const links = document.querySelectorAll('.link');
+// const sections = document.querySelectorAll('section');
 
-let activeLink = 0;
+// let activeLink = 0;
 
-links.forEach((link, i) => {
-    link.addEventListener('click', () => {
-        if(activeLink != i){
-            links[activeLink].classList.remove('active');
-            link.classList.add('active');
-            sections[activeLink].classList.remove('active');
+// links.forEach((link, i) => {
+//     link.addEventListener('click', () => {
+//         if(activeLink != i){
+//             link.classList.add('active');
 
-            setTimeout(() => {
-                activeLink = i;
-                sections[i].classList.add('active');
-            }, 1000);
-        }
-    })
-})
+//             setTimeout(() => {
+//                 activeLink = i;
+//                 sections[i].classList.add('active');
+//             }, 1000);
+//         }
+//     })
+// })
 
 let links = document.getElementsByClass("links");
 
@@ -35,7 +33,14 @@ links.addEventListener("mouseover", function(event) {
     }, 500);
 }, false);
 
+const handleFirstTab = (e) => {
+    if(e.key === 'Tab') {
+        document.body.classList.add('user-is-tabbing')
 
+        window.removeEventListener('keydown', handleFirstTab)
+        window.addEventListener('mousedown', handleMouseDownOnce)
+    }
+}
 
 
 
@@ -54,7 +59,8 @@ btn.addEventListener('click', function(event) {
 
 // adding an eventListener() to the submit form
 
-const form = document.getElementById('contact');
-form.addEventListener('submit', (event) => {
-    //handle data from the form
-})
+const f = document.createElement("form");
+document.body.appendChild(f);
+f.action = "/cgi-bin/some.cgi";
+f.method = "POST";
+f.submit();
