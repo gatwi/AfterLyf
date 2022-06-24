79efa8492d1a8c233920cf61fe0bf9c099c5123a
gatwi <faith.murithi@student.moringaschool.com>
2022-06-24 05:22:55 +0300

    Additional JS commit edits

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Bringing the navbar to life!
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -6,6 +8,9 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         if(this.scroll > 500){
+
+         // introducing functionality to the scroll-up-btn
+            
             $('.scroll-up-btn').addClass("scroll-up-btn");
         }else{
             $('.scroll-up-btn').removeClass("scroll-up-btn");
@@ -18,11 +23,23 @@ $(document).ready(function(){
 });
 
 
+const handleFirstTab = (e) => {
+    if(e.key === 'Tab') {
+      document.body.classList.add('user-is-tabbing')
+  
+      window.removeEventListener('keydown', handleFirstTab)
+      window.addEventListener('mousedown', handleMouseDownOnce)
+    }
+  
+  }
+
+
+
 
 // adding an eventListener() to the submit form
 
-// const f = document.createElement("form");
-// document.body.appendChild(f);
-// f.action = "/cgi-bin/some.cgi";
-// f.method = "POST";
-// f.submit();
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
