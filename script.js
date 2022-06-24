3b94b9f51b838e3e85924255bdc5d7c926d81833
gatwi <faith.murithi@student.moringaschool.com>
2022-06-24 18:35:48 +0300

    complete edits to the js file

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const getDiseases = (symptom) => {
+// communicating with the API
+
+const getDiseases = (symptoms) => {
     const options = {
         method: 'GET',
         headers: {
@@ -14,35 +16,37 @@ const getDiseases = (symptom) => {
 }
 
 const getResults = (results) => {
-    const resultSection = document.querySelector('#results') 
+
+const resultSection =  document.querySelector('#results')
 
     console.log(results)
     const ul = document.createElement('ul')
     results.forEach(res=>{
         const result = `
-        <div id="resultItem">
-        <h1>${res.Disease}</h1>
-        <p>Symptom 1: ${re.Symptom_1}</p>
-        <p>Symptom 2: ${res.Symptom_2}</p>
-        </div>
+         <div id="resultItem">
+                <h1>${res.Disease}</h1>
+                <p>Symptom 1: ${res.Symptom_1}</p>
+                <p>Symptom 2: ${res.Symptom_2}</p>
+                <p>Symptom 3: ${res.Symptom_3}</p>
+            </div>
         `
-        const li = document.createElement(li)
+        const li = document.createElement('li')
         li.innerHTML = result
         ul.appendChild(li)
+
     })
     resultSection.appendChild(ul)
 
-    
+
     const outputValue = document.querySelector("#answer")
-    outputValue.addEventListener("submit", function(e) {
+    outputValue.addEventListener("click", function(e) {
         e.preventDefault()
         console.log("hay fever")
-        const inputValue = document.querySelector("#query").value
+    const inputValue = document.querySelector("#query").value
+console.log(inputValue)
         getDiseases(inputValue)
     })
 }
-
-
 
 const init = () => {
     document.addEventListener("DOMContentLoaded", function() {
@@ -54,74 +58,45 @@ const init = () => {
 init()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // Bringing the navbar to life!
 
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         if(this.scrollY > 20){
-//             $('.navbar').addClass("sticky");
-//         }else{
-//             $('.navbar').removeClass("sticky");
-//         }
-//         if(this.scroll > 500){
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
 
-//          // introducing functionality to the scroll-up-btn
+         // introducing functionality to the scroll-up-btn
 
-//             $('.scroll-up-btn').addClass("scroll-up-btn");
-//         }else{
-//             $('.scroll-up-btn').removeClass("scroll-up-btn");
-//         }
-//     });
+            $('.scroll-up-btn').addClass("scroll-up-btn");
+        }else{
+            $('.scroll-up-btn').removeClass("scroll-up-btn");
+        }
+    });
 
-//     $('.scroll-up-btn').click(function(){
-//         $('html').animate({scrollTop: 0});
-//     })
-// });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    })
+});
 
 
-// const donor = document.getElementById("donorBtn")
-// console.log(donor)
+const donor = document.getElementById("donorBtn")
+console.log(donor)
 
-// donor.addEventListener("click", function(e) {
-//     e.preventDefault()
-// })
+donor.addEventListener("click", function(e) {
+    e.preventDefault()
+})
 
 
 
 // // adding an eventListener() to the submit form
 
-// const myForm = document.getElementById("myForm");
-// myForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-// })
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
 
 
